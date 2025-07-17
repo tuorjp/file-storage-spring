496354e7d5043ab4266789aa33bc5740af5e9b94
JP <jplmarotinho22@gmail.com>
2025-07-17 15:41:08 -0300

    FileStorageService: construtor que cria a pasta storage, caso a mesma não exista. FileStorageService.storeFile: método que salva um arquivo MultipartFile

---
+++ src/main/java/UEG/FileStorage/services/FileStorageService.java
@@ -0,0 +1,63 @@
+package UEG.FileStorage.services;
+
+import UEG.FileStorage.configs.FileStorageProperties;
+import UEG.FileStorage.exceptions.FileStorageCreationException;
+import UEG.FileStorage.exceptions.InvalidFileName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Service
+public class FileStorageService {
+  private final Path fileStorageLocation;
+
+  // construtor que inicializa o fileStorageLocation, com base no diretório especificado na classe de configuração
+  @Autowired //injeta o fileStorageProperties
+  public FileStorageService(FileStorageProperties fileStorageProperties) {
+    this.fileStorageLocation = Paths
+        .get(fileStorageProperties.getUploadDir())
+        .toAbsolutePath()
+        .normalize();
+
+    try {
+      Files.createDirectories(this.fileStorageLocation);
+    } catch (Exception e) {
+      throw new FileStorageCreationException("Não foi possível criar o diretório de armazenamento: " + e.getMessage());
+    }
+  }
+
+  // método que salva um arquivo no storage
+  public String storeFile(MultipartFile file) {
+    String originalFileName = StringUtils
+        .cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+
+    if(originalFileName.contains("..")) {
+      throw new InvalidFileName("Nome do arquivo inválido, renomeie e tente novamente.");
+    }
+
+    System.out.print(originalFileName);
+
+    // efetivamente salva o arquivo no diretório
+    try {
+      // constrói o caminho de destino, usando o método resolve
+      Path targetLocation = this.fileStorageLocation.resolve(originalFileName);
+      try (InputStream inputStream = file.getInputStream()) {
+        Files.copy(inputStream, targetLocation, StandardCopyOption.REPLACE_EXISTING);
+      }
+
+      // posteriormente o nome será tratado, por isso o retorno vai ser um nome que contém data e outras informações relevantes
+      return originalFileName;
+    } catch (IOException e) {
+      throw new FileStorageCreationException("Não foi possível salvar o arquivo");
+    }
+  }
+}
