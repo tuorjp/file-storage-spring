ca9939d06fe6212ec369a6b8c98d0a53faef0fe3
JP <jplmarotinho22@gmail.com>
2025-07-17 15:40:46 -0300

    Exceções para o FileStorageService

---
+++ src/main/java/UEG/FileStorage/exceptions/FileStorageCreationException.java
@@ -0,0 +1,7 @@
+package UEG.FileStorage.exceptions;
+
+public class FileStorageCreationException extends RuntimeException {
+  public FileStorageCreationException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/UEG/FileStorage/exceptions/InvalidFileName.java
@@ -0,0 +1,7 @@
+package UEG.FileStorage.exceptions;
+
+public class InvalidFileName extends RuntimeException {
+  public InvalidFileName(String message) {
+    super(message);
+  }
+}
